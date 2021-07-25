3b28986c7fd5f6a6fd5e1b49be8a402be8733bda
Toygar Aksoy <toygar.aksoy@gmail.com>
2021-07-25 21:19:48 +0300

    minor bug fix

--- tests/test_geo.py
+++ tests/test_geo.py
@@ -1,26 +1,13 @@
 import os, sys
-import tempfile
 
 import pytest
 import unittest
-from flask import Flask
 
 root = os.path.dirname(os.getcwd())
 sys.path.append(root)
 
 from fgeocoder import create_app
-# from fgeocoder.geocalc import geocalc
-# from fgeocoder import geocalc as geo
-# from fgeocoder.geocalc.geocalc import geocalc
 from fgeocoder.geocalc import geocalc as geo
-
-# print(os.path.isfile(os.path.join(os.getcwd(),"fgeocoder", "geocalc","mkad.geojson")))
-# print(os.path.abspath("../fgeocoder/mkad.geojson"))
-
-# @pytest.fixture
-# def client():
-#     client = geocalc.
-#     yield client
 
 
 class GeocoderTestCase(unittest.TestCase):
@@ -29,22 +16,16 @@ class GeocoderTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client()
 
-        # pass
-        # app.app.testing = True
-        # self.app = app.app.test_client()
-        # self.app = Flask(__name__)
-
     def tearDown(self) -> None:
         pass
 
     def test_response(self):
-        # app = Flask(__name__)
-        # app.register_blueprint(geocalc, url_prefix='/')
-        # web = app.test_client()
         response = self.client.get('/')
         assert response.status_code, 200
-        assert 'HTML' in response.data.decode('utf-8')
-        # self.assertEqual(response.headers['content-type'], "text/html; charset=utf-8")
+        assert '<h1>Hello. Welcome to geocalc</h1>' in response.data.decode(
+            'utf-8')
+        self.assertEqual(response.headers['content-type'],
+                         "text/html; charset=utf-8")
 
     def test_mkad_file(self):
         m_file = os.path.join(os.path.dirname(os.getcwd()), "fgeocoder",
@@ -86,14 +67,15 @@ class GeocoderTestCase(unittest.TestCase):
                      "А107, Moskovskaya oblast', Rusya, 142410",
                      "Pushkinsky District, Moskova Oblastı, Rusya, 141273",
                      "Ярославское ш., 47 км, Московская обл., 141273"]
-        confirms = ["<h1>1776.","<h1>36.7", "<h1>33.9", "<h1>32.0"]
+        confirms = ["<h1>1776.", "<h1>36.7", "<h1>33.9", "<h1>32.0"]
         for i in range(len(addresses)):
             response = self.client.get(f"/?address={addresses[i]}")
-            self.assertIn(confirms[i], response.data.decode('utf-8'), msg=f"address: {addresses[i]}")
+            self.assertIn(confirms[i], response.data.decode('utf-8'),
+                          msg=f"address: {addresses[i]}")
 
     def test_corner_cases(self):
-        # empty query
         for mark in "+.,-/_-|":
             query = f"/?address={mark}"
             response = self.client.get(query)
-            self.assertEqual(response.status, "200 OK", msg=f"Problematic query found: {mark}")
+            self.assertEqual(response.status, "200 OK",
+                             msg=f"Problematic query found: {mark}")
